d007ffbd45d7a13a4be41c362911c05acfb932e5
Andrey <andreeey.gg@gmail.com>
2025-05-06 12:33:13 +0200

    EXAM-32: Add custom.d.ts for images

---
+++ furniture-client/src/custom.d.ts
@@ -0,0 +1,20 @@
+declare module '*.svg' {
+    const contentSvg: string;
+    export default contentSvg;
+}
+
+declare module '*.png' {
+    const contentPng: string;
+    export default contentPng;
+}
+
+declare module '*.webp' {
+    const contentWebp: string;
+    export default contentWebp;
+}
+
+declare module '*.jpg' {
+    const contentJpg: string;
+    export default contentJpg;
+}
+
