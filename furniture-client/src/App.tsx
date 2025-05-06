d06fa8de9d1d91e069e895418ae4c227a3fae5d0
Andrey <andreeey.gg@gmail.com>
2025-05-06 12:29:35 +0200

    EXAM-32: Add new packages and config App.tsx

--- furniture-client/src/App.tsx
+++ furniture-client/src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+
+import theme from './assets/theme';
+import AppRoutes from './routes';
 
 
 function App() {
   return (
-    <>
-    </>
+      <div className="app">
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <BrowserRouter>
+                <AppRoutes />
+            </BrowserRouter>
+        </ThemeProvider>
+      </div>
   );
 }
 
