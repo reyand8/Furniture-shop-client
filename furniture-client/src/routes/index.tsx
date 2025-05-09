e1e27524dbec6b12bc08cb599e9dbe96085c31b1
Andrey <andreeey.gg@gmail.com>
2025-05-09 09:53:30 +0200

    EXAM-34: Add new packages, routes and update theme

--- furniture-client/src/routes/index.tsx
+++ furniture-client/src/routes/index.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import Profile from '../pages/Profile';
+import SignUp from '../pages/SignUp';
 
 
 const AppRoutes = () => (
     <Routes>
         <Route index element={<Home />} />
+        <Route path={'/profile'} element={<Profile />} />
+        <Route path={'/signup'} element={<SignUp />} />
     </Routes>
 );
 
