1ac67020ff08c20fbc990715d073fed4496b060b
Andrey <andreeey.gg@gmail.com>
2025-05-22 08:55:36 +0200

    EXAM-41: Add SingleProduct page and relative section

--- furniture-client/src/components/carousel-item/index.tsx
+++ furniture-client/src/components/carousel-item/index.tsx
@@ -1,5 +1,6 @@
 import { Typography, Box } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import { Link } from 'react-router-dom';
 
 import {
     CarouselImageBox,
@@ -11,42 +12,44 @@ import { IProduct } from '../../types/catalog.interface';
 
 
 const CarouselItem = (item: IProduct)=>  {
-    const { name, images, discountPrice, price, currency } = item;
+    const { id, name, images, discountPrice, price, currency } = item;
     return (
         <CarouselItemBox>
-            <CarouselImageBox className="carousel-image">
-                <Box
-                    component="img"
-                    src={images[0]}
-                    alt={name}
-                    sx={{
-                        width: '100%',
-                        height: '100%',
-                        objectFit: 'cover',
-                    }}
-                />
-            </CarouselImageBox>
-            <CarouselItemName variant="h5">{name}</CarouselItemName>
-            <Box sx={{height: '42px'}}>
-                {
-                    discountPrice ? (
-                        <Box>
-                            <Typography color="error">
-                                New lower price
-                            </Typography>
-                            <Typography variant="body1" color="text.secondary">
-                                {discountPrice} {currency}
-                            </Typography>
-                        </Box>
-                    ) : (
-                        <Box>
-                            <Typography variant="body1" color="text.secondary">
-                                {price} {currency}
-                            </Typography>
-                        </Box>
-                    )
-                }
-            </Box>
+            <Link to={`/single-product/${id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                <CarouselImageBox className="carousel-image">
+                    <Box
+                        component="img"
+                        src={images[0]}
+                        alt={name}
+                        sx={{
+                            width: '100%',
+                            height: '100%',
+                            objectFit: 'cover',
+                        }}
+                    />
+                </CarouselImageBox>
+                <CarouselItemName variant="h5">{name}</CarouselItemName>
+                <Box sx={{height: '42px'}}>
+                    {
+                        discountPrice ? (
+                            <Box>
+                                <Typography color="error">
+                                    New lower price
+                                </Typography>
+                                <Typography variant="body1" color="text.secondary">
+                                    {discountPrice} {currency}
+                                </Typography>
+                            </Box>
+                        ) : (
+                            <Box>
+                                <Typography variant="body1" color="text.secondary">
+                                    {price} {currency}
+                                </Typography>
+                            </Box>
+                        )
+                    }
+                </Box>
+            </Link>
             <IconCrossBox className="icon-cross">
                 <AddIcon fontSize="medium" />
             </IconCrossBox>
--- furniture-client/src/store/slice/catalog/catalog.slice.ts
+++ furniture-client/src/store/slice/catalog/catalog.slice.ts
@@ -11,7 +11,15 @@ const initialState: ICatalogState = {
     bestSellers: [],
     searchResults: [],
     allProducts: [],
+    relative: [],
+    singleProduct: null,
+
+    loadingSingle: false,
+    loadingRelative: false,
     loading: false,
+
+    errorSingle: null,
+    errorRelative: null,
     error: null,
 };
 
@@ -19,6 +27,19 @@ const catalogSlice = createSlice({
     name: 'catalog',
     initialState,
     reducers: {
+        fetchSingleProductRequest(state, _action: PayloadAction<string>) {
+            state.loadingSingle = true;
+            state.errorSingle = null;
+        },
+        fetchSingleProductSuccess(state, action: PayloadAction<IProduct>) {
+            state.loadingSingle = false;
+            state.singleProduct = action.payload;
+        },
+        fetchSingleProductFailure(state, action: PayloadAction<string>) {
+            state.loadingSingle = false;
+            state.errorSingle = action.payload || UNKNOWN_ERROR;
+        },
+
         fetchBestSellersRequest(state) {
             state.loading = true;
             state.error = null;
@@ -30,6 +51,19 @@ const catalogSlice = createSlice({
         fetchBestSellersFailure(state, action: PayloadAction<string>) {
             state.loading = false;
             state.error = action.payload || UNKNOWN_ERROR;
+        },
+
+        fetchRelativeRequest(state,  _action: PayloadAction<{ type: string }>) {
+            state.loadingRelative = true;
+            state.errorRelative = null;
+        },
+        fetchRelativeSuccess(state, action: PayloadAction<IProduct[]>) {
+            state.loadingRelative = false;
+            state.relative = action.payload;
+        },
+        fetchRelativeFailure(state, action: PayloadAction<string>) {
+            state.loadingRelative = false;
+            state.errorRelative = action.payload || UNKNOWN_ERROR;
         },
 
         searchRequest(state, _action: PayloadAction<string>) {
@@ -51,6 +85,8 @@ const catalogSlice = createSlice({
 });
 
 export const {
+    fetchSingleProductRequest, fetchSingleProductSuccess, fetchSingleProductFailure,
+    fetchRelativeRequest, fetchRelativeSuccess, fetchRelativeFailure,
     fetchBestSellersRequest, fetchBestSellersSuccess, fetchBestSellersFailure,
     searchRequest, searchSuccess, searchFailure, clearSearchResults
 } = catalogSlice.actions;
