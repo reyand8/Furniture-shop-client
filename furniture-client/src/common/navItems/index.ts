59e0d20f4a136bb7d39b62da5216e615579601bd
Andrey <andreeey.gg@gmail.com>
2025-05-23 18:16:32 +0200

    EXAM-42: Update paths

--- furniture-client/src/common/navItems/index.ts
+++ furniture-client/src/common/navItems/index.ts
@@ -2,13 +2,12 @@ import { INavItem } from '../../types/common.interface';
 import { PATHS } from '../../routes/paths';
 
 const {
-    HOME, CATALOG,
-    ABOUT, CONTACT
+    HOME, CATALOG, ABOUT, CONTACT
 } = PATHS;
 
 export const navItems: INavItem[] = [
-    { label: 'Home', path: `${HOME}` },
-    { label: 'Catalog', path: `${CATALOG}` },
-    { label: 'Contact', path: `${CONTACT}` },
-    { label: 'About Us', path: `${ABOUT}` },
+    { label: 'Home', path: HOME },
+    { label: 'Catalog', path: CATALOG },
+    { label: 'Contact', path: CONTACT },
+    { label: 'About Us', path: ABOUT },
 ];
